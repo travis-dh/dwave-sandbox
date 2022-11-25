c0c107403e4fe6f64a3b55e4e8d73ea87bf2c8be
niminuko <travishoward@protonmail.ch>
2022-11-25 08:21:48 -0600

    README.md updated and models.py added.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# dwave-sandbox
-A sandbox for toying with D-Wave samplers.
+# dwave-sandbox: A place for toying with D-Wave samplers.
+A repository is required for a developer to continue getting their free monthly minute to submit requests to D-Wave. The repository here thus serves as a sandbox.
+
+Inside is a Python script `models.py` which has a simple Ising model of three nodes.
+
+### The Ising model
+This Ising model has no external magnetic field presence, so each node has $h_{i} = 0$. The coupling strengths, however, are both $J_{ij}=1$. To change either of these terms, simply modify the `h` and/or `J` dictionaries.
+
+### Running the script
+A D-Wave account with a working API token (and D-Wave time!) is needed. First, install the dependencies by running `pip install -r requirements.txt`, then run `python models.py` to generate output `.txt` files containing the reponse data from the samplers.
---
+++ models.py
@@ -0,0 +1,62 @@
+# ----------------------------------------------------------------------
+# This program solves a simple Ising model with two coupling strengths J
+# both set to 1, with no external magnetic field term h on any of the 
+# three nodes.
+#
+# @author Travis Howard
+# @date 25 Nov 2022
+# ----------------------------------------------------------------------
+
+import dimod
+import neal
+from dwave.system import DWaveSampler, EmbeddingComposite
+
+# Define the system.
+h = {0: 0, 1: 0, 2: 0}
+J = {(0, 1): 1, (1, 2): 1}
+offset = 0.0
+
+def sim_anneal(h, J, num_reads=10):
+    '''
+    Run a neal.SimulatedAnnealingSampler given coupling
+    strengths and external magnetic field contributions.
+
+    :param h: External magnetic field(s).
+    :param J: Coupling strength(s).
+    :param num_reads: The number of samples taken.
+    :returns: A structured type which contains data of 
+              the sampler's response. 
+    '''
+    neal_sampler = neal.SimulatedAnnealingSampler()
+    neal_sampleset = neal_sampler.sample_ising(h, J, num_reads=num_reads)
+    return neal_sampleset
+
+def dwave_sampler(h, J, offset, num_reads=5000, label='models.py'):
+    '''
+    Run samples on a D-Wave computer with minor-embedding given
+    a binary quadratic model from coupling strengths and external
+    magnetic field contributions.
+
+    :param h: External magnetic field(s).
+    :param J: Coupling strength(s).
+    :param offset: An energy term which serves as a constant.
+    :param num_reads: The number of samples taken.
+    :returns: A structured type which constains data of 
+              the sampler's response.
+    '''
+    bqm = dimod.BinaryQuadraticModel(h, J, offset, 'SPIN')
+
+    sampler = EmbeddingComposite(DWaveSampler())
+    # This is where time gets used. Exercise caution!
+    sampleset = sampler.sample(bqm, num_reads=num_reads, label=label)
+    return sampleset
+
+if __name__ == '__main__':
+    # Simlated Annealing with output saved to file.
+    neal_sample_set = sim_anneal(h, J)
+    print(neal_sample_set, file=open('neal_output.txt' 'w'))
+
+    # D-Wave next. Time is used once `dwave_sampler` is called.
+    # Also save output to file.
+    dwave_sample_set = dwave_sampler(h, J, offset)
+    print(dwave_sample_set, file=open('dwave_output.txt', 'w'))
